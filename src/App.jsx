68608501a4330b4e80664589e2a823d6be610182
Caro Guerrero <caroguerrerosilvera@gmail.com>
2023-04-21 12:23:58 -0500

    feat: update movies data

--- src/App.jsx
+++ src/App.jsx
@@ -7,136 +7,128 @@ import { OrMovieTile, OrDialog, OrMovieForm } from "components/organisms";
 
 const MOVIES = [
   {
-    imageUrl: "https://image.tmdb.org/t/p/w500/9yBVqNruk6Ykrwc32qrK2TIE5xw.jpg",
-    title: "La Batalla de los Sexos",
+    imageUrl:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fWy0A3VojTCb0S2MKtEJjpquubF.jpg",
+    title: "Battle of the Sexes",
     releaseYear: 2017,
-    genres: ["Comedia", "Drama", "Deportes"],
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    genres: ["Comedy", "Drama", "History"],
+    rating: 6.8,
+    runtime: 121,
+    overview:
+      "The true story of the 1973 tennis match between World number one Billie Jean King and ex-champ and serial hustler Bobby Riggs.",
   },
   {
-    imageUrl: "https://image.tmdb.org/t/p/w500/9kg73Mg8WJKlB9Y2SAJzeDKAnuB.jpg",
+    imageUrl:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/aCIFMriQh8rvhxpN1IWGgvH0Tlg.jpg",
     title: "Tenet",
     releaseYear: 2020,
-    genres: ["Acción", "Ciencia ficción", "Suspenso"],
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    genres: ["Action", "Thriller", "Science Fiction"],
+    rating: 7.2,
+    runtime: 150,
+    overview:
+      "Armed with only one word - Tenet - and fighting for the survival of the entire world, the Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
   },
   {
-    imageUrl: "https://image.tmdb.org/t/p/w500/lIE7kfdLBRd0KENNtOaIqPPWNqh.jpg",
+    imageUrl:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hm58Jw4Lw8OIeECIq5qyPYhAeRJ.jpg",
     title: "Soul",
     releaseYear: 2020,
-    genres: ["Animación", "Aventura", "Comedia"],
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    genres: ["Animation", "Family", "Comedy", "Fantasy", "Drama"],
+    rating: 8.2,
+    runtime: 101,
+    overview:
+      "Joe Gardner is a middle school teacher with a love for jazz music. After a successful audition at the Half Note Club, he suddenly gets into an accident that separates his soul from his body and is transported to the You Seminar, a center in which souls develop and gain passions before being transported to a newborn child. Joe must enlist help from the other souls-in-training, like 22, a soul who has spent eons in the You Seminar, in order to get back to Earth.",
   },
   {
-    title: "El Club de la Pelea",
+    title: "Fight Club",
     releaseYear: 1999,
-    genres: ["Drama", "Suspenso"],
+    genres: ["Drama", "Thriller", "Comedy"],
     imageUrl: "https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    rating: 8.4,
+    runtime: 139,
+    overview:
+      'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
   },
   {
     title: "Matrix",
     releaseYear: 1999,
-    genres: ["Ciencia ficción", "Acción"],
+    genres: ["Science Fiction", "Action"],
     imageUrl: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    rating: 8.2,
+    runtime: 136,
+    overview:
+      "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
   },
   {
-    title: "Parásitos",
-    releaseYear: 2019,
-    genres: ["Drama", "Comedia", "Suspenso"],
+    title: "Parasite",
+    releaseYear: 2020,
+    genres: ["Drama", "Comedy", "Thriller"],
     imageUrl: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
-    rating: 7.0,
-    runtime: 120,
+    rating: 8.5,
+    runtime: 173,
     overview: "",
   },
   {
-    title: "El Club de los Poetas Muertos",
+    title: "Dead Poets Society",
     releaseYear: 1989,
     genres: ["Drama"],
-    imageUrl: "https://image.tmdb.org/t/p/w500/i4kPwXPlM1iy8Jf3S1uuLuwqQAV.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    imageUrl:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ai40gM7SUaGA6fthvsd87o8IQq4.jpg",
+    rating: 8.3,
+    runtime: 168,
+    overview:
+      "At an elite, old-fashioned boarding school in New England, a passionate English teacher inspires his students to rebel against convention and seize the potential of every day, courting the disdain of the stern headmaster.",
   },
   {
-    title: "Interestelar",
+    title: "Interstellar",
     releaseYear: 2014,
-    genres: ["Ciencia ficción", "Drama"],
+    genres: ["Science Fiction", "Drama"],
     imageUrl: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
-  },
-  {
-    title: "La La Land",
-    releaseYear: 2016,
-    genres: ["Comedia", "Drama", "Musical"],
-    imageUrl: "https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
-  },
-  {
-    title: "Blade Runner 2049",
-    releaseYear: 2017,
-    genres: ["Ciencia ficción", "Suspenso"],
-    imageUrl: "https://image.tmdb.org/t/p/w500/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    rating: 8.4,
+    runtime: 209,
+    overview:
+      "The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
   },
   {
     title: "El Laberinto del Fauno",
     releaseYear: 2006,
-    genres: ["Fantasía", "Drama", "Guerra"],
-    imageUrl: "https://image.tmdb.org/t/p/w500/ebSnODDg9lbsMIaWg2uAbjn7TO5.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
-  },
-  {
-    title: "Interestelar",
-    releaseYear: 2014,
-    genres: ["Ciencia ficción", "Drama"],
-    imageUrl: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    genres: ["Fantasy", "Drama"],
+    imageUrl:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/s8C4whhKtDaJvMDcyiMvx3BIF5F.jpg",
+    rating: 7.8,
+    runtime: 178,
+    overview:
+      "Living with her tyrannical stepfather in a new home with her pregnant mother, 10-year-old Ofelia feels alone until she explores a decaying labyrinth guarded by a mysterious faun who claims to know her destiny. If she wishes to return to her real father, Ofelia must complete three terrifying tasks.",
   },
   {
     title: "Jurassic Park",
     releaseYear: 1993,
-    genres: ["Ciencia ficción", "Acción", "Aventura"],
+    genres: ["Science Fiction", "Action", "Adventura"],
     imageUrl: "https://image.tmdb.org/t/p/w500/9i3plLl89DHMz7mahksDaAo7HIS.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    rating: 7.9,
+    runtime: 127,
+    overview:
+      "A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA. Before opening day, he invites a team of experts and his two eager grandchildren to experience the park and help calm anxious investors. However, the park is anything but amusing as the security systems go off-line and the dinosaurs escape.",
   },
   {
-    title: "El Hombre de Acero",
+    title: "Man of Steel",
     releaseYear: 2013,
-    genres: ["Acción", "Aventura", "Ciencia ficción"],
+    genres: ["Action", "Adventura", "Science Fiction"],
     imageUrl: "https://image.tmdb.org/t/p/w500/7rIPjn5TUK04O25ZkMyHrGNPgLx.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    rating: 6.6,
+    runtime: 143,
+    overview:
+      "A young boy learns that he has extraordinary powers and is not of this earth. As a young man, he journeys to discover where he came from and what he was sent here to do. But the hero in him must emerge if he is to save the world from annihilation and become the symbol of hope for all mankind.",
   },
   {
     title: "Coco",
     releaseYear: 2017,
-    genres: ["Animación", "Aventura", "Comedia"],
+    genres: ["Animation", "Adventura", "Comedy"],
     imageUrl: "https://image.tmdb.org/t/p/w500/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg",
-    rating: 7.0,
-    runtime: 120,
-    overview: "",
+    rating: 8.2,
+    runtime: 105,
+    overview:
+      "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
   },
 ];
 
@@ -175,6 +167,10 @@ function App() {
     console.log(movie);
   }
 
+  function handleCloseMovieForm() {
+    setOpenMovieForm(false);
+  }
+
   return (
     <div className="flex flex-col space-y-8 mx-auto max-w-4xl" id="root">
       <section className="relative overflow-hidden w-full py-10 px-16">
@@ -196,7 +192,7 @@ function App() {
 
           <div className="space-y-8 mt-8">
             <h1 className="uppercase text-white text-[40px] font-light">
-              FIND YOUR MOViE
+              FIND YOUR MOVIE
             </h1>
             <MlSearchForm
               placeholder="What do you want to watch?"
@@ -229,7 +225,7 @@ function App() {
       <section className="grid grid-cols-3 gap-6">
         {movies.map((movie) => (
           <OrMovieTile
-            {...movie}
+            movie={movie}
             onClick={handleClickMovie}
             onEdit={handleEditMovie}
             onDelete={handleDeleteMovie}
@@ -244,7 +240,7 @@ function App() {
       {openMovieForm && (
         <OrDialog
           title={selectedMovie ? "EDIT MOVIE" : "ADD MOVIE"}
-          onClose={() => setOpenMovieForm(false)}
+          onClose={handleCloseMovieForm}
         >
           <OrMovieForm />
         </OrDialog>
--- src/components/organisms/OrMovieTile/OrMovieTile.jsx
+++ src/components/organisms/OrMovieTile/OrMovieTile.jsx
@@ -4,15 +4,7 @@ import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
 
-export const OrMovieTile = ({
-  imageUrl,
-  title,
-  releaseYear,
-  genres,
-  onClick,
-  onEdit,
-  onDelete,
-}) => {
+export const OrMovieTile = ({ movie, onClick, onEdit, onDelete }) => {
   const [, setIsMenuOpen] = useState(false);
 
   function handleMenuOpen(event) {
@@ -43,31 +35,28 @@ export const OrMovieTile = ({
           className="bg-slate-800 text-white"
           overflow="auto"
           portal
-          id={`menu-${title}`}
+          id={`menu-${movie.title}`}
         >
-          <MenuItem
-            className="text-red"
-            onClick={() => onEdit({ imageUrl, title, releaseYear, genres })}
-          >
+          <MenuItem className="text-red" onClick={() => onEdit(movie)}>
             Edit
           </MenuItem>
-          <MenuItem
-            onClick={() => onDelete({ imageUrl, title, releaseYear, genres })}
-          >
-            Delete
-          </MenuItem>
+          <MenuItem onClick={() => onDelete(movie)}>Delete</MenuItem>
         </Menu>
-        <img className="h-[400px] w-full" src={imageUrl} alt={title} />
+        <img
+          className="h-[400px] w-full"
+          src={movie.imageUrl}
+          alt={movie.title}
+        />
       </div>
       <div className="flex justify-between">
         <div className="flex flex-col text-white space-y-2">
-          <h3 className="font-normal opacity-70">{title}</h3>
+          <h3 className="font-normal opacity-70">{movie.title}</h3>
           <div className="text-xs font-light opacity-50">
-            {genres.join(", ")}
+            {movie.genres.join(", ")}
           </div>
         </div>
         <span className="text-white opacity-70 text-xs border border-gray-100 self-start py-0.5 px-2 rounded-sm">
-          {releaseYear}
+          {movie.releaseYear}
         </span>
       </div>
     </div>
